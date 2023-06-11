480cd9e56364b6407c4330d1b273f175d9c28ea2
RoySolKo <royko@Roys-MacBook-Air.local>
2023-06-11 00:42:16 -0700

    added navigation and 2 buttons for info & settings

---
+++ Pages/InfoScreen.jsx
@@ -0,0 +1,34 @@
+import {SafeAreaView, StyleSheet, Text, Pressable} from 'react-native';
+import {Entypo} from '@expo/vector-icons';
+
+const InfoScreen = ({navigation}) => {
+  const infoButton = (
+    <Pressable
+      style={{
+        position: 'absolute',
+        top: 10,
+        right: 10,
+        width: 40,
+        height: 40,
+        backgroundColor: 'red',
+        borderRadius: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        zIndex: 1,
+      }}
+      onPress={() => navigation.navigate('GameScreen')}>
+      <Entypo name="info" size={24} color="black" />
+    </Pressable>
+  );
+
+  return (
+    <SafeAreaView>
+      {infoButton}
+      <Text>InfoScreen</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default InfoScreen;
